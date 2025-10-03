7d4181b8fc62e063ec761928c0446fff3eca464f
thinkpad <aaugustinwwezel@gmail.com>
2025-10-03 15:59:47 +0200

    Implement transpose and update checker filepath

--- devoir_1/solve.py
+++ devoir_1/solve.py
@@ -9,42 +9,40 @@ from collections import deque
     You are free to change the code below and to not use the precompleted part. The code is based on the high-level description at https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
     You can also define other sub-functions or import other datastructures from the collections library
 """
+
+
 def solve(adj):
     # adjacency of the graph and its transpose
     adj_out = adj
     adj_in = transpose(adj_out)
 
-    # number of nodes
     N = len(adj_in)
 
-    # is a node already visited?
-    visited = [False]*N
-    # list of node to process in the second step
-    L = []
+    visited = [False] * N  # Marquer tous les noeud "pas visité"
+
+    L = []  # Liste des noeuds à process à la seconde passe
+
     # queue of nodes to process with their associated status (i,False/True) i is the node index and True/False describes if we are appending the node to L or not when processing it
     q = deque()
 
     ### loop on every node and launch a visit of its descendants
     for x in range(N):
-        q.append((x,False))
+        q.append((x, False))
 
         while q:
-            x,to_append = q.pop()
+            x, to_append = q.pop()
 
             if to_append:
                 L.append(x)
 
             # TO COMPLETE, Note that you are free to completely change the body of this function, it is just a hint and several solutions are possible
 
-
     ### reverse the list to obtain the post-order
     L.reverse()
 
-
     ### find the strongly connected components
-    
-    # TO COMPLETE
 
+    # TO COMPLETE
 
     ### compute answer
     ans = 0
@@ -52,13 +50,20 @@ def solve(adj):
 
     return ans
 
+
 """
     Transpose the adjacency matrix
         Construct a new adjacency matrix by inverting all the edges: (x->y) becomes (y->x) 
 """
+
+
 def transpose(adj):
     adj_in = [list() for _ in range(len(adj))]
 
-    # TO COMPLETE
+    n = len(adj)
+
+    for i in range(n):
+        for node in adj[i]:
+            adj_in[node].append(i)
 
     return adj_in
